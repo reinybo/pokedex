c8b96be2f0e6576687a035c3d2f6eb6afb1a4880
Reiny Botros <reinybotros@Reinys-MacBook-Pro.local>
2022-12-14 21:58:39 -0800

    completed the rest of the routes

--- server.js
+++ server.js
@@ -7,6 +7,23 @@ const pokemons = require("./models/pokemon.js")
 
 const port = 3000
 
+app.use(express.static('public'))
+
+app.use(express.urlencoded({ extended: false }))
+
+app.use((req, res, next) => {
+    console.log("I run for all routes")
+    next()
+  })
+
+//include the method-override package
+const methodOverride = require("method-override")
+//...
+//after app has been defined
+//use methodOverride.  We'll be adding a query parameter to our delete form named _method
+app.use(methodOverride("_method"))
+
+
 // I is for INDEX
 
 app.get('/pokemon', function(req, res){
@@ -18,36 +35,43 @@ app.get('/pokemon', function(req, res){
 
 // N is for NEW
 
-app.get('pokemon/new', function(req, res){
-    res.send('new page')
+app.get('/pokemon/new', function(req, res){
+    res.render('new.ejs')
 })
 
 
 // D is for DELETE
 
 app.delete('/pokemon/:id', function(req, res){
-    res.send('delete route')
+    pokemons.splice(req.params.id, 1) //remove the item from the array
+    res.redirect("/pokemon") //redirect back to index route
 })
 
 
 // U is for UPDATE
 
 app.put('/pokemon/:id', function(req, res){
-    res.send('put route')
+    pokemons[req.params.id] = req.body //in our fruits array, find the index that is specified in the url (:indexOfFruitsArray).  Set that element to the value of req.body (the input data)
+    res.redirect("/pokemon") //redirect to the index page
 })
 
 
 // C is for CREATE
 
 app.post('/pokemon', function(req, res){
-    res.send('post route')
+    console.log(req.body)
+    pokemons.push(req.body)
+    res.redirect('/pokemon')
 })
 
 
 // E is for EDIT
 
 app.get('/pokemon/:id/edit', function(req, res){
-    res.send('edit page')
+    res.render('edit.ejs', {
+        pokemon: pokemons[req.params.id],
+        index: req.params.id
+    })
 })
 
 
